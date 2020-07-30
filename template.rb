ce15d4fbeb52657bfaa882475852489dba71ad15
Eoin Kelly <eoin@eoinkelly.info>
2020-07-31 11:05:23 +1200

    Improve Google Chrome install instructions

--- template.rb
+++ template.rb
@@ -19,6 +19,29 @@ def main # rubocop:disable Metrics/MethodLength
 
   EO_ROUTES_SNIPPET
 
+  google_chrome_install_instructions = <<~EO_CHROME_INSTALL
+    ### Installing Google Chrome
+
+    PDF rendering requires you to have Google Chrome installed both locally
+    (for development) and on your deployed environments. Here are some options
+    for installing chrome on server environments:
+
+    * Ubuntu Linux (suitable for AWS EC2 deployments):
+      ```bash
+      $ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+      $ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+      $ sudo apt-get update
+      $ sudo apt-get install google-chrome
+      ```
+    * Heroku
+      * https://github.com/heroku/heroku-buildpack-google-chrome
+    * macOS
+      * https://www.google.com/chrome/
+    * Windows
+      * https://www.google.com/chrome/
+
+  EO_CHROME_INSTALL
+
   append_to_file "README.md" do
     <<~EO_MESSAGE
 
@@ -27,24 +50,17 @@ def main # rubocop:disable Metrics/MethodLength
       This app renders PDFs by shelling out to Google Chrome via the Puppeteer
       Node API. See `app/services/pdf_rendering_service.rb` for details.
 
+      #{google_chrome_install_instructions}
     EO_MESSAGE
   end
+
 
   puts <<~EO_BANNER
     *************************************************************************
     TODO: Install Google Chrome
     *************************************************************************
 
-    PDF rendering requires you to have Google Chrome installed both locally
-    (for development) and on your deployed environments. Here are some options
-    for installing chrome on server environments:
-
-    Ubuntu Linux (suitable for AWS EC2 deployments):
-    $ apt-get install google-chrome
-
-    Heroku:
-    https://github.com/heroku/heroku-buildpack-google-chrome
-
+    #{google_chrome_install_instructions}
     *************************************************************************
     TODO: Delete/Edit the example controller
     *************************************************************************
