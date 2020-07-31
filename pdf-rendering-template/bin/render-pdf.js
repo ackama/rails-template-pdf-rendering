f99a4f8d3ddd1815d07d59c52bda5e38a66335e1
Eoin Kelly <eoin@eoinkelly.info>
2020-07-31 12:22:00 +1200

    Update node script
    * Allow setting custom chrome path
    * Run it through eslint & prettier
    * Set `--no-sandbox` to make it work on Chrome

--- pdf-rendering-template/bin/render-pdf.js
+++ pdf-rendering-template/bin/render-pdf.js
@@ -8,34 +8,45 @@
  *
  */
 
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
-const [,, rawInputPath, outputPath] = process.argv;
+const [, , rawInputPath, outputPath] = process.argv;
 const inputPath = `file://${rawInputPath}`;
 
-console.log("Input path:", inputPath);
-console.log("Output path:", outputPath);
+console.log('Input path:', inputPath);
+console.log('Output path:', outputPath);
 
 (async () => {
-  const browser = await puppeteer.launch();
+  // Allow setting the path to Chrome via environment variable. This is usually
+  // not required but is useful for folks doing dev in WSL on Windows
+  const chromePath = process.env.PDF_RENDERING_PATH_TO_CHROME || undefined;
+
+  // `--no-sandbox` is required on Heroku - see
+  // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-on-heroku
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    executablePath: chromePath
+  });
+
   try {
     const page = await browser.newPage();
 
-    console.log("Starting PDF conversion");
-    await page.goto(inputPath, { waitUntil: "networkidle2" });
+    console.log('Starting PDF conversion');
+    await page.goto(inputPath, { waitUntil: 'networkidle2' });
 
-    // https://github.com/GoogleChrome/puppeteer/blob/v1.4.0/docs/api.md#pagepdfoptions
+    // Puppeteer docs: https://pptr.dev/
     await page.pdf({
       path: outputPath,
-      format: "A4"
+      format: 'A4'
     });
 
-    console.log("PDF conversion complete");
+    console.log('PDF conversion complete');
   } finally {
     await browser.close();
   }
 })().catch(error => {
   console.error(error);
-  
+
   process.exit(1);
 });
+
